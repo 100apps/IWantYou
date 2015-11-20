9967f3f68a1b375910c9fde605b5d1b4a2b90995
广峰 <gf@gfzj.us>
2015-11-20 19:21:53 +0800

    第一版可用版本

---
+++ makeAWish.js
@@ -0,0 +1,81 @@
+var http = require("https");
+var cheerio=require("cheerio");
+var fs = require('fs');
+
+var smtpUser="yunteng.ma@houpix.com";
+var smtpPwd="**********";
+var smtpServer="smtp.qiye.163.com";
+
+var startUrl="https://github.com/search?q=location%3Ashanghai&ref=searchresults&type=Users&utf8=%E2%9C%93";
+
+var email   = require("emailjs/email");
+var server  = email.server.connect({
+   user:    smtpUser, 
+   password:smtpPwd, 
+   host:    smtpServer
+});
+var text=fs.readFileSync("email.txt",'utf-8');
+var sendedTmp=fs.readFileSync("log.txt",'utf-8').split("\n");
+var sended=[];
+for(var i=0;i<sendedTmp.length;i++){
+  sended[sendedTmp[i]]=1;
+}
+console.log(sended.length+"已发");
+
+console.log("开工");
+
+getEmails(1);
+
+function getEmails(page) {
+  console.log("开始抓取第 "+page+" 页");
+  http.get(startUrl+"&p="+page, function(res) {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on("end", function() {
+    var $ = cheerio.load(data);
+
+    $(".email").each(function(i, e) {
+      var email=$(e).text();
+      var div=$(e).parent().parent().parent().parent();
+      $("a",div).remove();
+      $("ul",div).remove();
+      var username=div.text().trim();
+      if(email.indexOf("@")>0&&username.length>1)
+        sendEmail(email,username);
+      });
+ 
+    if(!$(".next_page").hasClass("disabled")){
+      //next page
+      getEmails(page+1);
+    }
+    });
+  }).on("error", function() {
+    console.log("error "+page);
+  });
+}
+
+
+function sendEmail(toEmail,toName){
+  if(sended[toEmail]){
+    console.log(toEmail+" 已发");
+    return;
+  }
+  var message = {
+     text:    text.replace("${name}",toName), 
+     from:    "马云腾 <"+smtpUser+">", 
+     to:      toName+" <"+toEmail+">",
+     subject: "来自马云腾的邀请函",
+  };
+
+  server.send(message, function(err, message) { 
+    if(err){
+      console.log(err);
+    }else{
+      console.log("sended to "+toName+" <"+toEmail+">\n"+message.text);
+      fs.appendFile("log.txt",toEmail+"\n");
+      sended[toEmail]=1;
+    }
+  });
+}
